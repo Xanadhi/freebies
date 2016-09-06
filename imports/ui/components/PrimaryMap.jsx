dfa00ee349f80dc5e4ff303d2b6167989d854628
Andy Tran <andytran@ehealthinnovation.org>
2016-09-06 16:13:25 -0400

    credit added

--- imports/ui/components/PrimaryMap.jsx
+++ imports/ui/components/PrimaryMap.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+// Adapted from https://github.com/dburles/meteor-google-maps-react-example/blob/master/googlemaps-react.jsx
+
 export default React.createClass({
   mixins: [ReactMeteorData],
   componentDidMount() {
