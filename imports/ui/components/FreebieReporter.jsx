e5bbd6769ad0fdfb9ae80bc1e7b1224805f4e13b
Andy Tran <transient.tran@gmail.com>
2016-09-18 17:50:40 -0400

    basic input field added

---
+++ imports/ui/components/FreebieReporter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class FreebieReporter extends Component {
+  render() {
+    return (
+      <input type="text" />
+      )
+  }
+}
--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import NavBar from '../components/NavBar.jsx';
 import PrimaryMap from '../components/PrimaryMap.jsx';
+import FreebieReporter from '../components/FreebieReporter.jsx';
 
 // import MapContainer from './MapContainer.jsx';
 
@@ -12,6 +13,11 @@ export default class App extends Component {
       <div>
       <NavBar />
         <div className="container-fluid">
+          <div className="row">
+            <div className="col-xs-12">
+              <FreebieReporter />
+            </div>
+          </div>
           <div className="row">
             <div className="col-xs-12">
                 <PrimaryMap />
