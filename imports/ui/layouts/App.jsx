78102f12eb3692393aa29ede70d2a11b6dc4c8ea
Andy Tran <transient.tran@gmail.com>
2016-10-01 09:57:47 -0400

    refactored code to be more component-like and putting the map and form into a map container

--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 
 import NavBar from '../components/NavBar.jsx';
-import PrimaryMap from '../components/PrimaryMap.jsx';
-import FreebieForm from '../components/FreebieForm.jsx';
-
-// import MapContainer from './MapContainer.jsx';
+import MapContainer from './MapContainer.jsx';
 
 export default class App extends Component {
 
@@ -13,14 +10,7 @@ export default class App extends Component {
       <div>
       <NavBar />
         <div className="container-fluid">
-          <div className="row">
-            <div className="col-xs-12 map-column">
-                <PrimaryMap />
-            </div>
-            <div className="post-button">
-              <FreebieForm />
-            </div>
-          </div>
+            <MapContainer />
         </div>
       </div>
     );
--- imports/ui/layouts/MapContainer.jsx
+++ imports/ui/layouts/MapContainer.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 import PrimaryMap from '../components/PrimaryMap.jsx';
+import FreebieForm from '../components/FreebieForm.jsx';
 
 export default class MapContainer extends Component {
   render() {
     return (
-      <div className="map-container">
-        <PrimaryMap />
+      <div>
+        <div className="col-xs-12 map-column">
+          <PrimaryMap />
+        </div>        
+        <div className="post-button">
+          <FreebieForm />
+        </div>
       </div>
       )
   }
