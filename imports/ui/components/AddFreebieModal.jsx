968333dbcfb22c59f57eaddb5467ef4d34eead3e
trandromeda <transient.tran@gmail.com>
2016-10-07 18:41:02 -0400

    created new component for modal with comments as placeholders

---
+++ imports/ui/components/AddFreebieModal.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import { Freebies } from '../../api/freebies/freebies.js';
+
+export default class AddFreebieModal extends Component {
+
+// ask user if they want to add current location as a Freeie
+  //* if their current location is correct, allow them to enter the right address/click on the map
+
+// asks for some basic information:
+  // sponsor
+  // type of freebie
+  // extra desc?
+
+// inserts Freebie into the collection
+
+render() {
+  return (
+
+    )
+}
+
+}
