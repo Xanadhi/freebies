3473e2f5311ae0905527b035d33898cffbdd3a04
trandromeda <transient.tran@gmail.com>
2016-10-19 16:44:50 -0400

    basic form built on, need to make the category into a dropdown next

--- imports/ui/components/AddFreebieModal.jsx
+++ imports/ui/components/AddFreebieModal.jsx
@@ -18,12 +18,18 @@ export default class AddFreebieModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      name: '',
+      category: '',
+      desc: '',
+      sponsor: '',
       address: ''
     }
   }
 
-  handleAddressChange(e) {
-    this.setState({address: e.target.value});
+  handleChange(name, e) {
+    var change = {};
+    change[name] = e.target.value
+    this.setState(change);
   }
 
   handleSubmit(e) {
@@ -33,14 +39,50 @@ export default class AddFreebieModal extends Component {
 
   render() {
     return (
-      <div>
+      <div className="form-group">
       <form className="add-form" onSubmit={this.handleSubmit.bind(this)}>
+        <label for="name">Name</label>
+        <input 
+          type="text"
+          id="name" 
+          value={this.state.name}
+          className="form-control"
+          onChange={this.handleChange.bind(this, 'name')} 
+        />
+        <label for="category">Category</label>
         <input 
           type="text" 
-          value={this.state.address} 
-          onChange={this.handleAddressChange.bind(this)} 
+          name="category"
+          value={this.state.category} 
+          className="form-control"
+          onChange={this.handleChange.bind(this, 'category')} 
         />
-        <input type="submit" value="Post" />
+        <label for="desc">Description</label>
+        <input 
+          type="text"
+          name="desc"
+          value={this.state.desc}
+          className="form-control" 
+          onChange={this.handleChange.bind(this, 'desc')} 
+        />
+        <label for="sponsor">Sponsor</label>
+        <input 
+          type="text"
+          name="sponsor"
+          value={this.state.sponsor} 
+          className="form-control"
+          onChange={this.handleChange.bind(this, 'sponsor')} 
+        />
+        <label for="address">Address</label>
+        <input 
+          type="text"
+          name="address"
+          value={this.state.address}
+          className="form-control"
+          onChange={this.handleChange.bind(this, 'address')} 
+        />        
+
+        <input type="submit" className="btn btn-default" value="Post" />
       </form>
 
       <button onClick={this.props.close}>close</button>
