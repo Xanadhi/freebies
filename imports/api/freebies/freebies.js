1df66a811004c533044a4d6dbf2802bd71c2caf6
trandromeda <transient.tran@gmail.com>
2016-10-28 14:02:38 -0400

    added two more packages and created basic schema

--- imports/api/freebies/freebies.js
+++ imports/api/freebies/freebies.js
@@ -1,3 +1,12 @@
 import { Mongo } from 'meteor/mongo';
 
 export const Freebies = new Mongo.Collection('freebies');
+
+Freebies.schema = new SimpleSchema({
+  name: {type: String},
+  desc: {type: String},
+  sponsor: {type: String},
+  latLng: {type: Object}
+});
+
+Freebies.attachSchema(Freebies.schema);
