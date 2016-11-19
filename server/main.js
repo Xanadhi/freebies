2cf9f080a3da164895f96f06d1d8526eccc983b2
trandromeda <transient.tran@gmail.com>
2016-11-19 10:06:23 -0500

    removing old import lines

--- server/main.js
+++ server/main.js
@@ -1,9 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import '/imports/startup/server';
 
-import '../imports/api/freebies/freebies.js';
-import '../imports/api/freebies/server/publications.js';
-
 Meteor.startup(() => {
   // code to run on server at startup
 });
